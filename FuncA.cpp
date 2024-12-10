eef2294f12dded7b9f51d3ea8a26fedaadfa371c
DmytroZor <zorenkodmytro03@gmail.com>
2024-12-10 12:54:02 +0000

    Finalize FuncA implementation with comments

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,7 +1,22 @@
-#include "func.h"
+#include "Func.h"
+#include <cmath> // for pow() and factorial()
 
-// Initial implementation: Returns 0
-double Func::FuncA() {
-    return 0;
+double factorial(int num) {
+    double result = 1;
+    for (int i = 1; i <= num; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+// Implementation of FuncA using Taylor series for sin(x)
+double Func::FuncA(double x, int n) {
+    double sum = 0;
+    for (int i = 0; i < n; ++i) {
+        int power = 2 * i + 1;
+        double term = pow(-1, i) * pow(x, power) / factorial(power);
+        sum += term;
+    }
+    return sum;
 }
 
--- FuncA.h
+++ FuncA.h
@@ -3,8 +3,12 @@
 
 class Func {
 public:
-    // Calculates the result of the trigonometric function (initially returns 0)
-    double FuncA();
+    /**
+     * @param x: angle in radians
+     * @param n: number of terms in the Taylor series
+     * @return Sum of the first n terms of the Taylor series for sin(x)
+     */
+    double FuncA(double x, int n);
 };
 
 #endif
