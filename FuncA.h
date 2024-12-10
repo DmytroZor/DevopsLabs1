6af540e28460bfc1207e00c76ea977f147f2eb46
DmytroZor <zorenkodmytro03@gmail.com>
2024-12-10 12:33:42 +0000

    func.h, fucn.cpp, main.cpp

---
+++ FuncA.h
@@ -0,0 +1,11 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+class Func {
+public:
+    // Calculates the result of the trigonometric function (initially returns 0)
+    double FuncA();
+};
+
+#endif
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "func.h"
+
+int main() {
+    Func func;
+    std::cout << "FuncA result: " << func.FuncA() << std::endl;
+    return 0;
+}
+
+
